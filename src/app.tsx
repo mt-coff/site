9cb6b3f0a517e7109be2ddb7ac90392d709bceb6
mt-coff <mt.coff.00@gmail.com>
2020-12-26 14:20:04 +0900

    :tada:

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import { h, Fragment } from "preact";
+import { Introduction } from "./components/Introduction";
+
+export function App() {
+  return (
+    <>
+      <Introduction />
+    </>
+  );
+}
---
+++ src/components/Introduction/index.tsx
@@ -0,0 +1,9 @@
+import { h, FunctionComponent } from "preact";
+
+export const Introduction: FunctionComponent = () => {
+  return (
+    <section>
+      <h1>mt_coff</h1>
+    </section>
+  );
+};
